822ab1db918a53fee48aef0ccb7eee0b34b23e54
BasicOne <Ntahimperafrancis@gmail.com>
2025-09-29 01:07:02 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "project_name" {
 variable "location" {
   description = "Azure region for deployment."
   type        = string
-  default     = "westeurope"
+  default     = "francecentral"
 }
 
 variable "vm_admin_username" {
@@ -20,5 +20,30 @@ variable "vm_admin_password" {
   description = "Password for the VM administrator (MUST be secure in production)."
   type        = string
   # NOTE: Replace this with a secure password or use Key Vault/SSH keys in production.
-  default     = "P@ssw0rd123456" 
+  default     = "P@ssw0rd123456"
+}
+
+# Fichier pour déclarer les variables
+
+variable "arm_client_id" {
+  description = "The Client ID for the Azure Service Principal"
+  type        = string
+  sensitive   = true # Marqué comme sensible pour ne pas l'afficher dans les logs plan/apply
+}
+
+variable "arm_client_secret" {
+  description = "The Client Secret for the Azure Service Principal"
+  type        = string
+  sensitive   = true
+}
+
+variable "arm_tenant_id" {
+  description = "The Tenant ID for the Azure Service Principal"
+  type        = string
+  sensitive   = true
+}
+
+variable "arm_subscription_id" {
+  description = "The Azure Subscription ID"
+  type        = string
 }
